77fd7e95bfdda86b66e7e5b5c4fa37397a1842ca
Juanma <juanaco1.p@gmail.com>
2020-04-05 12:29:58 +0200

    copy files to directory

--- Organizer.pm
+++ Organizer.pm
@@ -26,7 +26,9 @@ sub organize {
         die("$self->{directory} not exists");
     }
 
-    if(! $self->_check_directory_has_images()) {
+    my @files = $self->_get_directory_images();
+
+    if(! scalar(@files)) {
         die("No images files in $self->{directory}");
     }
 
@@ -34,10 +36,18 @@ sub organize {
         die();
     }
 
+    foreach (@files) {
+        print "File $_\n";
+        my $destination = $self->_get_file_dest_directory($_);
+        print "Copy to $destination\n";
+        copy($_, $destination) or die "Copy failed: $!";
+    }
+
     # loop files
         # select first file
         # compose destination
             # destination directory (hour)
+            # destination name (time)
             # is there a file in the same time?
                 # append _1 to filename
                 # filename
@@ -53,13 +63,13 @@ sub _check_directory_exists {
     return -d $self->{directory};
 }
 
-sub _check_directory_has_images {
+sub _get_directory_images {
     my $self = shift;
 
     my $directory_files_glob = $self->{directory} . "/*.{" . join(',', FILE_EXTENSIONS) . '}';
     my @files = glob( $directory_files_glob );
 
-    return scalar(@files);
+    return @files;
 }
 
 sub _confirm {
@@ -81,21 +91,25 @@ sub _confirm {
 #     print "Total files: " . scalar(@files) . "\n";
 # }
 
-# sub get_file_dest_directory {
-#     my $exifTool = new Image::ExifTool;
-#     my $directory_name = 'unknown_date';
-#     my $self = shift;
+sub _get_file_dest_directory {
+    my $self = shift;
+    my $exifTool = new Image::ExifTool;
+    my $full_path = "$self->{directory}/unknown_date";
 
-#     my $info = $exifTool->ImageInfo($_);
+    my $info = $exifTool->ImageInfo($_);
 
-#     if($info->{CreateDate}) {
-#         my ($date, $time) = split(' ', $info->{CreateDate});
-#         my @date_components = split(':', $date);
+    if($info->{CreateDate}) {
+        my ($date, $time) = split(' ', $info->{CreateDate});
+        my @date_components = split(':', $date);
 
-#         $directory_name = join('-', @date_components);
-#     }
+        $full_path = "$self->{directory}/" . join('-', @date_components);
 
-#     return "$self->{directory}/$directory_name";
-# }
+        if(! -d $full_path) {
+            mkdir($full_path);
+        }
+    }
+
+    return "$full_path/" . basename($_);
+}
 
 1;
